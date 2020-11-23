8096c8692d9b581ced8f2a959ec4fe8d17700cc9
stanleyshuang <stanleyshuang@qnap.com>
2020-11-24 06:31:14 +0800

    lab integration

--- _build/chocho-ndr1/env.sh
+++ _build/chocho-ndr1/env.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 base_dir=$(dirname "$0")
 export ni="6: USB 10/100/100 LA"
-export cnc_ip="192.168.1.200"
+export cnc_ip="192.168.1.199"
 export ip_prx="192.168.1"
-export tgt_bgn="201"
-export tgt_end="211"
+export tgt_bgn="200"
+export tgt_end="207"
 
 echo "ni=\"$ni\" ip_prx=\"$ip_prx\" cnc_ip=\"$cnc_ip\" tgt_bgn=\"$tgt_bgn\" tgt_end=\"$tgt_end\" vagrant "
