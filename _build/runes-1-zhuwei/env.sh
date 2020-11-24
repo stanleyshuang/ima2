c48562e254724e0a6de1a82bc0b6af6dabf60bb7
stanleyshuang <stanleyshuang@qnap.com>
2020-11-24 08:56:26 +0800

    lab integration v3

--- _build/runes-1-zhuwei/env.sh
+++ _build/runes-1-zhuwei/env.sh
@@ -4,6 +4,6 @@ export ni="en0: Wi-Fi (AirPort)"
 export cnc_ip="172.20.10.7"
 export ip_prx="172.20.10"
 export tgt_bgn="8"
-export tgt_end="18"
+export tgt_end="13"
 
 echo "ni=\"$ni\" ip_prx=\"$ip_prx\" cnc_ip=\"$cnc_ip\" tgt_bgn=\"$tgt_bgn\" tgt_end=\"$tgt_end\" vagrant "
