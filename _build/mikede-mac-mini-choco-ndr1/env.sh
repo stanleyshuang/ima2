d692437afc072d3b734ab35797836c7da82fa06e
Choco <choco@Mikede-Mac-mini.local>
2020-11-26 15:32:39 +0800

    environment settings

---
+++ _build/mikede-mac-mini-choco-ndr1/env.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+base_dir=$(dirname "$0")
+export ni="en0: 乙太網路"
+export cnc_ip="192.168.1.199"
+export ip_prx="192.168.1"
+export tgt_bgn="200"
+export tgt_end="205"
+
+echo "ni=\"$ni\" ip_prx=\"$ip_prx\" cnc_ip=\"$cnc_ip\" tgt_bgn=\"$tgt_bgn\" tgt_end=\"$tgt_end\" vagrant "
